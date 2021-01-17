f71f850372f47e5a36144e841c0146199004f028
Jordan Kidwell <jkid2023@gmail.com>
2021-01-17 13:23:36 -0800

    final sum of rows problem complete

--- PreWorkCodeChallenges/Program.cs
+++ PreWorkCodeChallenges/Program.cs
@@ -6,10 +6,11 @@ namespace PreWorkCodeChallenges
     {
         static void Main(string[] args)
         {
-            int[] someArray = new int[] { 8,10,14 };
+            // int[] someArray = new int[] { 8,10,14 };
             // numberGame();
             // leapYearCalc();
-            perfectSequence(someArray); 
+            // perfectSequence(someArray); 
+            sumOfRows();
         }
         static void numberGame()
         {
@@ -71,6 +72,20 @@ namespace PreWorkCodeChallenges
             {
                 Console.WriteLine("Heck no..");
             }
+        }
+        static int[] sumOfRows()
+        {
+            int[,] multiArray = new int[3, 5] { { 12, 13, 14, 15, 16 }, { 30, 31, 32, 33, 34 }, { 64, 65, 66, 67, 68 } };
+            int[] singularArray = new int[multiArray.GetLength(0)];
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                {
+                    singularArray[i] += multiArray[i, j];
+                }      
+            }
+            Console.WriteLine($"[{string.Join(", ", singularArray)}]");
+            return singularArray;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# prework-codechallenges
+# Prework-CodeChallenges
+
+## Problem #1: Array Max Result
+- **Problem Domain**: Given an array, select a number that exists between 1-10, and output the total score. 
+- You should create a method that takes in both an array of integers and the integer the user selected. Output the array to the console, ask the user to select a number from these 5. After selected, output "score" of the number chosen.
+- ![ArrayMax](/../preworkcc1.png)
+
+## Problem #2: Leap Year Calculator 
+- **Problem Domain**: Given a year, report if it is or is not a leap year.
+- ![Is-A-Leap-Year](/../isaleapyear.png)
+- ![Not-A-Leap-Year](/../prework-leapyear.png)
+
+## Problem #3: Perfect Sequence
+- **Problem Domain**: Given an array, return "Yes" if the sequence is considered to be a perfect sequence, otherwise return "no".
+- ![Perfect-Sequence](/../perfect-sequence.png)
+
+## Problem #4: Sum Of Rows
+- **Problem Domain**: Given a matrix of integers, return the sum of each row in a single dimensional array.
+- ![Sum-Of-Rows](/../sumofrows.png)
